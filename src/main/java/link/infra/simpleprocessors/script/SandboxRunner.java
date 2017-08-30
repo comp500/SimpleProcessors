bb778cf9d58bc0dc5fc37acd7a31203d93e0cc79
comp500 <comp500@users.noreply.github.com>
2017-08-30 12:19:49 +0100

    Copy some classes from ecmacraft

--- src/main/java/link/infra/simpleprocessors/script/SandboxRunner.java
+++ src/main/java/link/infra/simpleprocessors/script/SandboxRunner.java
@@ -1,19 +1,65 @@
 package link.infra.simpleprocessors.script;
 
 import java.util.concurrent.Executors;
-
 import delight.nashornsandbox.NashornSandbox;
 import delight.nashornsandbox.NashornSandboxes;
+import link.infra.simpleprocessors.script.apis.Console;
+import link.infra.simpleprocessors.script.apis.Require;
 
 public class SandboxRunner {
-	
+
 	private NashornSandbox sandbox;
-	
+
 	public SandboxRunner() {
 		sandbox = NashornSandboxes.create();
 		sandbox.setMaxCPUTime(1000); // prevent while(true)
 		sandbox.setExecutor(Executors.newSingleThreadExecutor());
 		sandbox.setDebug(true);
+	}
+
+	public void init() {
+		// inject "native" functions into Nashorn
+		sandbox.inject("console", new Console());
+		sandbox.inject("require_native", new Require());
+		injectRequireFix();
+	}
+
+	/*
+		var module = {exports: {}};
+		var exports = module.exports;
+		var require = (function(require_native) {
+			return function(str) {
+				var req = require_native.getnative(str);
+				if (req == null) {
+					var codeString = require_native.get(str);
+					if (codeString) {
+						var oldModule = module;
+						var oldExports = exports;
+						module = {exports: {}};
+						exports = module.exports;
+						eval(codeString);
+						var fin = module.exports;
+						module = oldModule;
+						exports = oldExports;
+						return fin;
+					} else {
+						return null;
+					}
+				} else {
+					return req;
+				}
+			}
+		})(require_native);
+		delete require_native;
+	 */
+
+	public void injectRequireFix() {
+		// load require override with eval() to allow module loading
+		sandbox.eval("var module={exports:{}},exports=module.exports,require=function(d){return function(a){var b=d.getnative(a);if(null==b){var c=d.get(a);return c?(a=module,b=exports,module={exports:{}},exports=module.exports,eval(c),c=module.exports,module=a,exports=b,c):null}return b}}(require_native);delete require_native;");
+	}
+	
+	public Object evalCode(String code) {
+		return sandbox.eval(code);
 	}
 
 }
---
+++ src/main/java/link/infra/simpleprocessors/script/apis/Require.java
@@ -0,0 +1,14 @@
+package link.infra.simpleprocessors.script.apis;
+
+public class Require {
+	
+	public Object getnative(String msg) {
+		return null;
+	}
+	
+	public String get(String msg) {
+		// todo fix to read from dirs
+		return null;
+	}
+
+}
