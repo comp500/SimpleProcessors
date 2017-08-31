38ae0d8c2cfc2402e349f908ef570d7801a5739a
comp500 <comp500@users.noreply.github.com>
2017-08-31 23:05:27 +0100

    Fix model registration

--- src/main/java/link/infra/simpleprocessors/proxy/ClientProxy.java
+++ src/main/java/link/infra/simpleprocessors/proxy/ClientProxy.java
@@ -3,9 +3,12 @@ package link.infra.simpleprocessors.proxy;
 import link.infra.simpleprocessors.ModBlocks;
 import link.infra.simpleprocessors.ModItems;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
+@Mod.EventBusSubscriber(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
 
 	@Override
