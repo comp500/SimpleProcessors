600d9b6b668060a3f501f35e2ab931f28eee5b4a
comp500 <comp500@users.noreply.github.com>
2017-09-03 22:16:41 +0100

    Storage Map impl

--- src/main/java/link/infra/simpleprocessors/blocks/programmer/ProgrammerTileEntity.java
+++ src/main/java/link/infra/simpleprocessors/blocks/programmer/ProgrammerTileEntity.java
@@ -1,6 +1,12 @@
 package link.infra.simpleprocessors.blocks.programmer;
 
+import java.util.HashMap;
+
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 
 public class ProgrammerTileEntity extends TileEntity {
@@ -9,5 +15,79 @@ public class ProgrammerTileEntity extends TileEntity {
 		// If we are too far away from this tile entity you cannot use it
 		return !isInvalid() && playerIn.getDistanceSq(pos.add(0.5D, 0.5D, 0.5D)) <= 64D;
 	}
+
+    private NBTTagCompound storageMap; 
+
+    public String getFile(String file) {
+        return storageMap.getString(file);
+    }
+    
+    public HashMap<String, Integer> getFileList() {
+    	if (storageMap == null || storageMap.hasNoTags()) {
+    		return new HashMap<String, Integer>();
+    	} else {
+    		HashMap<String, Integer> tempMap = new HashMap<String, Integer>();
+    		for (String s : storageMap.getKeySet()) {
+    			tempMap.put(s, storageMap.getString(s).length());
+    		}
+    		return tempMap;
+    	}
+    }
+
+    public void setFile(String file, String data) {
+        this.storageMap.setString(file, data);
+        markDirty();
+        if (world != null) {
+            IBlockState state = world.getBlockState(getPos());
+            world.notifyBlockUpdate(getPos(), state, state, 3);
+        }
+    }
+
+    @Override
+    public NBTTagCompound getUpdateTag() {
+        // getUpdateTag() is called whenever the chunkdata is sent to the
+        // client. In contrast getUpdatePacket() is called when the tile entity
+        // itself wants to sync to the client. In many cases you want to send
+        // over the same information in getUpdateTag() as in getUpdatePacket().
+    	NBTTagCompound nbtTag = new NBTTagCompound();
+        this.writeToNBT(nbtTag);
+        return writeToNBT(new NBTTagCompound());
+    }
+
+    @Override
+    public SPacketUpdateTileEntity getUpdatePacket() {
+        // Prepare a packet for syncing our TE to the client. Since we only have to sync the stack
+        // and that's all we have we just write our entire NBT here. If you have a complex
+        // tile entity that doesn't need to have all information on the client you can write
+        // a more optimal NBT here.
+        NBTTagCompound nbtTag = new NBTTagCompound();
+        this.writeToNBT(nbtTag);
+        return new SPacketUpdateTileEntity(getPos(), 1, nbtTag);
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, SPacketUpdateTileEntity packet) {
+        // Here we get the packet from the server and read it into our client side tile entity
+        this.readFromNBT(packet.getNbtCompound());
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound compound) {
+        super.readFromNBT(compound);
+        if (compound.hasKey("storage")) {
+        	storageMap = compound.getCompoundTag("storage");
+        } else {
+            storageMap = new NBTTagCompound();
+        }
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+        super.writeToNBT(compound);
+        if (!storageMap.hasNoTags()) {
+            compound.setTag("storage", storageMap);
+        }
+        return compound;
+    }
 
 }
