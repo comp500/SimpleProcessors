a2260a32b4b6f80c2fa24e009507f9b49470ddf1
comp500 <comp500@users.noreply.github.com>
2017-09-06 22:56:46 +0100

    Add buttons and new function

--- src/main/java/link/infra/simpleprocessors/gui/BrowserTab.java
+++ src/main/java/link/infra/simpleprocessors/gui/BrowserTab.java
@@ -2,12 +2,14 @@ package link.infra.simpleprocessors.gui;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
 import link.infra.simpleprocessors.SimpleProcessors;
 import link.infra.simpleprocessors.blocks.programmer.ProgrammerContainer;
 import link.infra.simpleprocessors.blocks.programmer.ProgrammerTileEntity;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
 
 public class BrowserTab extends ProgrammerTab {
@@ -24,9 +26,6 @@ public class BrowserTab extends ProgrammerTab {
 	public void initGui(ProgrammerGui programmerGui, ProgrammerTileEntity te, ProgrammerContainer container, FontRenderer fontRenderer) {
 		//fileList = te.getFileList();
 		fileList = new HashMap<String, Integer>();
-		for (int i = 0; i < 50; i++) {
-			fileList.put("hi" + i, 10);
-		}
 		fileList.put("hello", 3);
 		fileList.put("hi", 10);
 		slotList = new BrowserScrollingList(programmerGui.mc, fileList, programmerGui.getGuiLeft() + 5, programmerGui.getGuiTop() + 33, programmerGui.width, programmerGui.height);
@@ -41,12 +40,30 @@ public class BrowserTab extends ProgrammerTab {
 	
 	@Override
 	public void actionPerformed(GuiButton button) {
-		slotList.actionPerformed(button);
+		//slotList.actionPerformed(button);
+		if (button.id == 1) {
+			String name = "unnamed";
+			int i = 1;
+			while (fileList.containsKey(name)) {
+				name = "unnamed_" + i;
+				i++;
+			}
+			fileList.put(name, 0);
+			slotList.refresh(fileList);
+		}
 	}
 	
 	@Override
 	public void handleMouseInput(ProgrammerGui programmerGui, int x, int y) throws IOException {
 		slotList.handleMouseInput(x, y);
+	}
+	
+	@Override
+	public void initButtons(List<GuiButton> buttonList, ProgrammerGui gui) {
+		int guiLeft = gui.getGuiLeft();
+		int guiTop = gui.getGuiTop();
+		buttonList.add(new GuiButton(0, guiLeft + 115, guiTop + 33, 56, 17, I18n.format("button." + SimpleProcessors.MODID + ".programmer.tab.browser.open.name")));
+		buttonList.add(new GuiButton(1, guiLeft + 115, guiTop + 52, 56, 17, I18n.format("button." + SimpleProcessors.MODID + ".programmer.tab.browser.new.name")));
 	}
 	
 }
