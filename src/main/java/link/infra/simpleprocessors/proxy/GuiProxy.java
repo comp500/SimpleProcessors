cbaf9fc0429fff0eb3ccc63473a2fd33365de784
comp500 <comp500@users.noreply.github.com>
2017-09-09 13:38:35 +0100

    Register case gui in proxy

--- src/main/java/link/infra/simpleprocessors/proxy/GuiProxy.java
+++ src/main/java/link/infra/simpleprocessors/proxy/GuiProxy.java
@@ -1,5 +1,8 @@
 package link.infra.simpleprocessors.proxy;
 
+import link.infra.simpleprocessors.blocks.processorcase.CaseContainer;
+import link.infra.simpleprocessors.blocks.processorcase.CaseGui;
+import link.infra.simpleprocessors.blocks.processorcase.CaseTileEntity;
 import link.infra.simpleprocessors.blocks.programmer.ProgrammerContainer;
 import link.infra.simpleprocessors.blocks.programmer.ProgrammerGui;
 import link.infra.simpleprocessors.blocks.programmer.ProgrammerTileEntity;
@@ -20,6 +23,13 @@ public class GuiProxy implements IGuiHandler {
 				return new ProgrammerContainer(player.inventory, (ProgrammerTileEntity) te);
 			}
 		}
+		if (ID == CaseGui.GUI_ID) {
+			BlockPos pos = new BlockPos(x, y, z);
+			TileEntity te = world.getTileEntity(pos);
+			if (te instanceof CaseTileEntity) {
+				return new CaseContainer(player.inventory, (CaseTileEntity) te);
+			}
+		}
 		return null;
 	}
 
@@ -31,6 +41,14 @@ public class GuiProxy implements IGuiHandler {
 			if (te instanceof ProgrammerTileEntity) {
 				ProgrammerTileEntity containerTileEntity = (ProgrammerTileEntity) te;
 				return new ProgrammerGui(containerTileEntity, new ProgrammerContainer(player.inventory, containerTileEntity));
+			}
+		}
+		if (ID == CaseGui.GUI_ID) {
+			BlockPos pos = new BlockPos(x, y, z);
+			TileEntity te = world.getTileEntity(pos);
+			if (te instanceof CaseTileEntity) {
+				CaseTileEntity containerTileEntity = (CaseTileEntity) te;
+				return new CaseGui(new CaseContainer(player.inventory, containerTileEntity));
 			}
 		}
 		return null;
