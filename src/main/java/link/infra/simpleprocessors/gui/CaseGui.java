6382df4797cdde014e566802b1eee847647bc1ab
comp500 <comp500@users.noreply.github.com>
2017-09-09 12:02:14 +0100

    Add case gui

---
+++ src/main/java/link/infra/simpleprocessors/gui/CaseGui.java
@@ -0,0 +1,25 @@
+package link.infra.simpleprocessors.gui;
+
+import link.infra.simpleprocessors.SimpleProcessors;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.inventory.Container;
+import net.minecraft.util.ResourceLocation;
+
+public class CaseGui extends GuiContainer {
+	
+	private static final ResourceLocation background = new ResourceLocation(SimpleProcessors.MODID, "textures/gui/case.png");
+
+	public CaseGui(Container inventorySlotsIn) {
+		super(inventorySlotsIn);
+		
+		xSize = 176;
+		ySize = 166;
+	}
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+		mc.getTextureManager().bindTexture(background);
+        drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
+	}
+
+}
